c3ca90be0a2287fefac07046b30708e75b9e7eed
Anderson Rodrigues de Sousa <undersonrs1@gmail.com>
2020-10-15 09:26:21 -0300

    Create par_ou_impar.py

---
+++ par_ou_impar.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um número e te direi se ele é par ou ímpar : '))
+if num % 2 == 0:
+    print('O número digitado é par com certeza!')
+else:
+    print('O número digitado é ímpar!')

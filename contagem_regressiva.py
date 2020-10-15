dd70b59420ef71e72891ce8dd6681c61540cffcd
Anderson Rodrigues de Sousa <undersonrs1@gmail.com>
2020-10-15 12:52:54 -0300

    Update contagem_regressiva.py

--- contagem_regressiva.py
+++ contagem_regressiva.py
@@ -1,8 +1,11 @@
-import time
 
-for regressiva in range (10, 0, -1):
-    print(regressiva)
-    a = time.time()
-    time.sleep(1)
+import time #importa o módulo do contador de tempo 
+
+for regressiva in range (10, -1, -1): # especifica valor inicial, final e sua iteração
+    print(regressiva) # exibe a contagem regressiva
+    a = time.time() # marca o inicio da contagem de tempo
+    time.sleep(1) # aguarda 1 segundo e prossegue a contagem regressiva
+
+print ('\nContagem regressiva concluída!\n')
 
 

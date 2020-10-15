973e912c6649ad90bc215bbe0ae688b3b7f50314
Anderson Rodrigues de Sousa <undersonrs1@gmail.com>
2020-10-15 09:21:26 -0300

    Create multaVelocidade.py

---
+++ multaVelocidade.py
@@ -0,0 +1,7 @@
+vel = int(input('Qual foi a velocidade que passou naquela rua? : '))
+if vel >= 80:
+    print('Você excedeu o limite da velocidade')
+    print('Terá de pagar uma multa no valor de R$180,00')
+else:
+    print('Você estava dentro do limite de velocidade permitido para esta rua. Parabéns!')
+print ('-----FIM------')

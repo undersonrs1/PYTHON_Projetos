ffb1cfecfaea8af9f1144e203cbba51ec1f48b12
Anderson Rodrigues de Sousa <undersonrs1@gmail.com>
2020-10-17 02:41:11 -0300

    Update M_ou_F.py

--- M_ou_F.py
+++ M_ou_F.py
@@ -1,12 +1,15 @@
+
 a = 0
+
 while a < 2 or a > 3:
     print('\nQual o seu sexo?')
     print('[2] Masculino')
     print('[3] Feminino\n')
-    a=int(input('Digite aqui a sua opção : '))
+    a= int(input('Digite aqui a sua opção : '))
 
 if a == 2:
     print('\nConfirmando: Masculino\n')
+
 elif a == 3:
     print('\nConfirmando: Feminino\n')
 

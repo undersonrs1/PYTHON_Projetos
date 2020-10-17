b21e79178927668380bd852e33c0e0d5a2041d0a
Anderson Rodrigues de Sousa <undersonrs1@gmail.com>
2020-10-17 03:28:15 -0300

    Create testeFunção.py

---
+++ testeFunção.py
@@ -0,0 +1,9 @@
+def  potencia (base , expoente ):
+    resultado = base ** expoente
+    return resultado
+
+b = float (  input ( "\nEntre com uma base :  " )  )
+exp = float (  input ( "\nEntre com um expoente :  " )  )
+
+pot = potencia (b ,  exp )
+print ( "\nResultado  de " , b ,  " elevado  a " ,  exp ,  " :  " ,  pot, end='\n\n' )

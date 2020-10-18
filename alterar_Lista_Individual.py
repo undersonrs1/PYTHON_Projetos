3c3c5267eb9d6546760d7f7ae0c552cc82cdcf9c
Anderson Rodrigues de Sousa <undersonrs1@gmail.com>
2020-10-18 18:51:27 -0300

    new_files

---
+++ alterar_Lista_Individual.py
@@ -0,0 +1,17 @@
+a = [1,2,3,4,5]
+
+b = a
+
+b[0] = 0
+
+print('\nAs listas não se alteram, pois são colocadas em igualdade!')
+print('\na : {}'.format (a))
+print('b : {}'.format (b))
+
+b = a[:]
+
+b[0] = 1
+
+print('\nNeste segundo caso, as listas ficam diferentes, pois b recebe apenas uma cópia dos itens de a em [:]')
+print('\na : {}'.format (a))
+print('b : {}'.format (b), end="\n\n")
---
+++ enumerar_Lista.py
@@ -0,0 +1,6 @@
+lista = [1,2,3,4,5,6,7,8,9,10]
+
+for a,b in enumerate (lista):
+    print ('O índice {} da lista é {}'.format(a,b))
+
+print ('\nFIM DA LEITURA DA LISTA\n')

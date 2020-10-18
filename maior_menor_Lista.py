35ebd279039c323569651175b43ebd3319860d9b
Anderson Rodrigues de Sousa <undersonrs1@gmail.com>
2020-10-18 04:36:52 -0300

    Create maior_menor_Lista.py

---
+++ maior_menor_Lista.py
@@ -0,0 +1,10 @@
+lista = []
+
+for a in range (0,5):
+    v = int (input('Digite um valor para a lista : '))
+    lista.append(v)
+    lista.sort()
+    
+print (lista[0])
+print(lista[4])
+print(lista)

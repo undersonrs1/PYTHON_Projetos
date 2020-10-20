979d4e57421fec462384632c10f9392cf0109548
Anderson Rodrigues de Sousa <undersonrs1@gmail.com>
2020-10-20 00:38:39 -0300

    update files

---
+++ funcao_imprime_linha.py
@@ -0,0 +1,18 @@
+def lin():
+    print ('-' * 30)
+
+
+for a in range(0 , 1):
+    lin()
+    print('a' * 30)
+    lin()
+    print()
+    print()
+    lin()
+    print('b' * 30)
+    lin ()
+
+
+
+
+

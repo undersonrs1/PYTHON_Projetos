138a7ad1ce976407ab7875a1fa522b07c11a31c8
Anderson Rodrigues de Sousa <undersonrs1@gmail.com>
2020-10-20 12:20:28 -0300

    Create funcao_Contador.py

---
+++ funcao_Contador.py
@@ -0,0 +1,12 @@
+def contador(inicio, fim, incremento):
+    for a in range (inicio, fim, incremento):
+        print (a, end=' ')
+
+# Programa Principal
+
+i = int (input('\nDigite o número inicial de sua contagem : '))
+f = int (input('\nDigite o número final de sua contagem : '))
+inc = int (input('\nDigite o incremento de sua contagem : '))
+
+cont = contador (i, f, inc)
+

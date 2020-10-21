936689b3f9835e73d0c008fbe93486d5dcb67c70
Anderson Rodrigues de Sousa <undersonrs1@gmail.com>
2020-10-20 21:39:58 -0300

    add_files

---
+++ class_MyList.py
@@ -0,0 +1,7 @@
+import random
+class MyList(list):
+    def choice(self):
+        return random.choice(self)
+
+
+ 
---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+def fib(n):
+    if n == 0:
+        return 0
+
+    elif n == 1:
+        return 1
+
+    elif n>=2:
+        res = fib(n-1) + fib(n-2)
+        return res
+
+
+
+
+
+
+

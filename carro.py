8eb958f0eb75393caa697e79718bac63d137351c
Anderson Rodrigues Sousa <65787205+undersonrs@users.noreply.github.com>
2020-11-07 16:27:55 -0300

    Update carro.py

--- carro.py
+++ carro.py
@@ -3,5 +3,6 @@ tempo = int(input('Digite quantos anos tem o seu carro : '))
 if tempo >=5:
     print('Seu carro está velho!')
 else:
-    print('Seu carro está novo, muito antiquado!)
+    print('Seu carro está novo')
+    
 
